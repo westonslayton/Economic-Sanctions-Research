88b83086af54f03ee781816be68b9f94bac6da58
Paul Dilly <paul.dilly@duke.edu>
2023-02-25 00:53:54 -0500

    figuring it out

--- imf.py
+++ imf.py
@@ -1,6 +1,18 @@
 import requests
 import os
 import pandas as pd
+from imfpy import searches
+#from imfpy.retrievals import dots
+import time
+import ret
+
+#for handling API timeouts
+#time.sleep(5)
+
+#getting country codes and writing to csv
+#df = searches.country_codes()
+#cwd = os.getcwd() + "/data"
+#df.to_csv(os.path.join(cwd, "country_codes.csv"), header = True)
 
 #root url
 url = 'http://dataservices.imf.org/REST/SDMX_JSON.svc/'
@@ -8,20 +20,22 @@ url = 'http://dataservices.imf.org/REST/SDMX_JSON.svc/'
 #part of url that accesses exports from country to partner beginning at start date (yr)
 param = [('dataset', 'DOT'),
          ('freq', 'M'),
-         ('country', 'ZM'),
+         ('country', 'IT'),
          ('series', 'TXG_FOB_USD'),
-         ('partner', 'ZW'),
+         ('partner', 'FR'),
          ('start', '?startPeriod=2022')]
 series = '.'.join([i[1] for i in param[1:5]])
 
 #form rest of url after root
 key = f'CompactData/{param[0][1]}/{series}{param[-1][1]}'
-
+#key = f'CompactData/DOT/M..TXG_FOB_USD..?startPeriod=2022'
+#key = f'CompactData/DOT/A.FR.TXG_FOB_USD.IT.?startPeriod=2022'
 #combine API url with key specific to data request
 r = requests.get(f'{url}{key}').json()
 
 #data portion of results
 data = r['CompactData']['DataSet']['Series']
+#print(data)
 
 #create pandas data frame from the observations
 data_list = [[obs.get('@TIME_PERIOD'), obs.get('@OBS_VALUE')]
@@ -33,88 +47,45 @@ df["Country"] = param[2][1]
 df["Partner"] = param[4][1]
 
 #save cleaned dataframe as a csv file and save to data folder
-#df.to_csv('export_ex.csv', header = True)
+df.to_csv('export_ex.csv', header = True)
 cwd = os.getcwd() + "/data"
-df.to_csv(os.path.join(cwd, "export_ex.csv"), header = True)
-
+df.to_csv(os.path.join(cwd, "tryme.csv"), header = True)
 
 
 #second data set
 param = [('dataset', 'DOT'),
-         ('freq', 'M'),
-         ('country', 'IT'),
-         ('series', 'TXG_FOB_USD'),
-         ('partner', 'FR'),
-         ('start', '?startPeriod=2022')]
+        ('freq', 'M'),
+        ('country', 'IT'),
+        ('series', 'TXG_FOB_USD'),
+        ('partner', 'FR+BR+CL+CO'),
+        ('start', '?startPeriod=2022')]
 series = '.'.join([i[1] for i in param[1:5]])
 
+
 key = f'CompactData/{param[0][1]}/{series}{param[-1][1]}'
+
 
 r = requests.get(f'{url}{key}').json()
 data = r['CompactData']['DataSet']['Series']
 
+
 data_list = [[obs.get('@TIME_PERIOD'), obs.get('@OBS_VALUE')]
-    for obs in data['Obs']]
+   for obs in data['Obs']]
 df2 = pd.DataFrame(data_list, columns = ["Date", "Value (Millions USD)"])
+
 
 df2["Country"] = param[2][1]
 df2["Partner"] = param[4][1]
 
+
 #combining both data sets
 df = pd.concat([df, df2])
 
+
 cwd = os.getcwd() + "/data"
-df.to_csv(os.path.join(cwd, "export_ex2.csv"), header = True)
+df.to_csv(os.path.join(cwd, "checkmate.csv"), header = True)
 
 
 
 
 
-#getting list of country codes
-key = 'DataStructure/DOT'  # Method/series
-dimension_list = requests.get(f'{url}{key}').json()\
-            ['Structure']['KeyFamilies']['KeyFamily']\
-            ['Components']['Dimension']
-
-key = f"CodeList/{dimension_list[1]['@codelist']}"
-code_list = requests.get(f'{url}{key}').json()\
-	    ['Structure']['CodeLists']['CodeList']['Code']
-#for code in code_list:
-    #print(f"{code['Description']['#text']}: {code['@value']}")
-
-#loop to get exports for every country pair
-big_df = pd.DataFrame()
-for country in code_list:
-    for partner in code_list:
-        if country['@value'] != partner['@value']:
-            param = [('dataset', 'DOT'),
-            ('freq', 'M'),
-            ('country', country['@value']),
-            ('series', 'TXG_FOB_USD'),
-            ('partner', partner['@value']),
-            ('start', '?startPeriod=2022')]
-            series = '.'.join([i[1] for i in param[1:5]])
-            key = f'CompactData/{param[0][1]}/{series}{param[-1][1]}'
-            #r = requests.get(f'{url}{key}').json()
-            #data = r['CompactData']['DataSet']['Series']
-            #data_list = [[obs.get('@TIME_PERIOD'), obs.get('@OBS_VALUE')]
-                #for obs in data['Obs']]
-            #df = pd.DataFrame(data_list, columns = ["Date", "Value (Millions USD)"])
-            data = requests.get(f'{url}{key}').json()
-            df = pd.DataFrame({[[obs.get('@TIME_PERIOD'), obs.get('@OBS_VALUE')]
-                    for obs in s['Obs']]
-                for s in data['CompactData']['DataSet']['Series']})
-            df["Country"] = country['Description']['#text']
-            df["Partner"] = partner['Description']['#text']
-            big_df = pd.concat(big_df, df)
-
-
-
-#testing new way of getting dfs; works indiv but in loop???       
-url = 'http://dataservices.imf.org/REST/SDMX_JSON.svc/CompactData/DOT/M.US.TXG_FOB_USD.W00.?startPeriod=2022'
-data = requests.get(url).json()
-usexp = pd.DataFrame(data['CompactData']['DataSet']['Series']['Obs'])
-usexp.columns = ['date','value (millions USD)']
-usexp["country"] = "US"
-usexp["partner"] = "WOO"
-usexp.to_csv(os.path.join(cwd, "us_exp.csv"), header = True)
